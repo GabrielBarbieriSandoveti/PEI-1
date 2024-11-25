d5f013325389ab2d844586a4e94e3d4d13d0de0e
José Gabriel <josegabrielp.leite@gmail.com>
2024-11-25 17:16:46 -0400

    Resolvendo alguns problemas nas duas procedures, e as deixando mais completas e a prova de erro. Sendo uma das resoluções que, agora para saida de entrada de bolsas no relatorio não terão como base apenas o agendamento, mas sim, confirmando em tabelas: Bolsas (para entrada) e Transfusao (para saida).

--- DataBase/Procedures.sql
+++ DataBase/Procedures.sql
@@ -1,56 +1,62 @@
---procedure (de cadastro de doadores ou triagens
 --use DBHemoware
---vai se chamar procedure.sql
+--PROCEDURES.SQL
 
 --Essa procedure vai gerar relatorio de entrada e saida das bolsas com datas especificadas pelo usuario:
-
 create procedure RelatorioEspecificoDeBolsas
 	@DataInicio DATE,
 	@DataFinal DATE
-	
 AS
 BEGIN
 	SELECT 
-		format(DataColeta, 'dd/mm/yyyy') AS 'Semana',
+		CONVERT(VARCHAR(10), B.DataColeta, 120) AS 'Data',
 		SUM(CASE WHEN TipoAgendamento = 'Coleta' THEN 1 ELSE 0 END) AS 'Entradas de Bolsas',
-		SUM(CASE WHEN TipoAgendamento = 'Transfusao' THEN 1 ELSE 0 END) AS 'Saida de Bolsas',
+		SUM(CASE WHEN T.IDBolsa IS NOT NULL THEN 1 ELSE 0 END) AS 'Saida de Bolsas',
 		
-		SUM(CASE WHEN TipoAgendamento = 'Coleta' THEN 1 ELSE 0 END) - SUM(CASE WHEN TipoAgendamento = 'Transfusao' THEN 1 ELSE 0 END) AS 'Total'
+		SUM(CASE WHEN TipoAgendamento = 'Coleta' THEN 1 ELSE 0 END) - SUM(CASE WHEN T.IDBolsa IS NOT NULL THEN 1 ELSE 0 END) AS 'Total'
 		
 		FROM Bolsas B
-		JOIN
-			Agendamento A ON B.IDDoador = A.IDDoador 
-			
-		WHERE
-			DataColeta BETWEEN @DataInicio AND @DataFinal
-		GROUP BY format(DataColeta, 'dd/mm/yyyy')
-		ORDER BY format(DataColeta, 'dd/mm/yyyy')
+		
+		INNER JOIN Agendamento A ON B.IDDoador = A.IDDoador 
+		LEFT JOIN Transfusao T ON B.IDBolsa = T.IDBolsa
+		
+		WHERE 
+			B.DataColeta BETWEEN @DataInicio AND @DataFinal
+		GROUP BY CONVERT(VARCHAR(10), B.DataColeta, 120)
+		ORDER BY CONVERT(VARCHAR(10), B.DataColeta, 120);
 END;
 
 --Executando essa procedure, colocando as datas que deseja o relatorio:
-EXEC RelatorioEspecificoDeBolsas @DataInicio = '01/01/2024', @DataFinal = '31/12/2024';
+EXEC RelatorioEspecificoDeBolsas @DataInicio = '2024-01-01', @DataFinal = '2025-01-02';
 
 
 
 --Essa procedure vai gerar relatorio de entrada e saida das bolsas semanalmente:
-
 create procedure RelatorioSemanalDeBolsas
 AS
 BEGIN
 	SELECT 
-		format(DATEADD(WEEK, DATEDIFF(WEEK, 0, DataColeta), 0), 'dd/mm/yyyy') AS 'Inicio da Semana',
-		format(DATEADD(DAY, 6, DATEADD(WEEK, DATEDIFF(WEEK, 0, DataColeta), 0)), 'dd/mm/yyyy') AS 'Final da Semana',
-		SUM(CASE WHEN A.TipoAgendamento = 'Coleta' then 1 else 0 end) AS 'Entradas de Bolsas',
-		SUM(CASE WHEN A.TipoAgendamento = 'Transfusao' then 1 else 0 end) AS 'Saida de Bolsas',
+		CONVERT(VARCHAR(10), DATEADD(DAY, -DATEPART(WEEKDAY, MIN(DataColeta)) + 1, MIN(DataColeta)), 103) AS 'Inicio da Semana',
+		CONVERT(VARCHAR(10), DATEADD(DAY, -DATEPART(WEEKDAY, MIN(DataColeta)) + 7, MIN(DataColeta)), 103) AS 'Final da Semana',
+		SUM(CASE WHEN A.TipoAgendamento = 'Coleta' THEN 1 ELSE 0 END) AS 'Entradas de Bolsas',
+		SUM(CASE WHEN T.IDBolsa IS NOT NULL THEN 1 ELSE 0 END) AS 'Saida de Bolsas',
 		
-		SUM(CASE WHEN A.TipoAgendamento = 'Coleta' THEN 1 ELSE 0 END) - SUM(CASE WHEN A.TipoAgendamento = 'Transfusao' THEN 1 ELSE 0 END) AS 'Total'
+		SUM(CASE WHEN A.TipoAgendamento = 'Coleta' THEN 1 ELSE 0 END) - SUM(CASE WHEN T.IDBolsa IS NOT NULL THEN 1 ELSE 0 END) AS 'Total'
+		
 	FROM Bolsas B
-	
-	JOIN 
-		Agendamento A ON B.IDDoador = A.IDDoador
-	GROUP BY DATEDIFF(WEEK, 0, DataColeta)
-	ORDER BY DATEDIFF(WEEK, 0, DataColeta);
+	JOIN Agendamento A ON B.IDDoador = A.IDDoador
+	LEFT JOIN Transfusao T ON B.IDBolsa = T.IDBolsa
+	GROUP BY DATEADD(DAY, -DATEPART(WEEKDAY, DataColeta) + 1, DataColeta)
+	ORDER BY DATEADD(DAY, -DATEPART(WEEKDAY, DataColeta) + 1, DataColeta);
 END;
 
 --Executando essa procedure:
 EXEC RelatorioSemanalDeBolsas;
+
+
+
+
+
+
+
+
+
