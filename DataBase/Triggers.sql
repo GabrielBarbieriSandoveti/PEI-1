5c1683c80f558ccd95188958d3c42395c4a71cba
José Gabriel <josegabrielp.leite@gmail.com>
2024-11-25 17:10:25 -0400

    Resolvi todos os erros da trigger ValidarAgendamento e criei uma nova, onde essa trigger faz com que se o resultado do cadastrado na pretriagem e triagem for Aprovado, ele ira para a tabela doadores automaticamente, sem a necessidade de preencher a tabela manualmente.

--- DataBase/Triggers.sql
+++ DataBase/Triggers.sql
@@ -1,13 +1,11 @@
---Para possiveis novas implementacoes no sistema, sera adicionado triggers: 
--- 1: para ajustar o estoque de sangue após entrada/saida de uma bolsa (lembrando que vai ser necessario excluir a view 'VW_EstoqueSangue' e criar uma tabela de estoque de bolsas 
---		(nao deixando apenas na tabela bolsa). 
--- 2: para verificar se quantidade de bolsas de sangue no estoque esta baixo e notificar se estiver)
+--Para possiveis novas implementacoes no sistema, sera adicionado trigger: para verificar se quantidade de bolsas de sangue no estoque esta baixo e notificar se estiver)
 
 --use DBHemoware
---vai se chamar triggers.sql
+--TRIGGERS.SQL
+--Criando as triggers do sistema.
+
 
 --Essa trigger vai previnir erros, bloqueando doadores que nao passaram na pretriagem de fazerem coleta de sangue:
-
 create trigger ValidarAgendamento ON Agendamento
 INSTEAD OF INSERT 
 AS
@@ -15,7 +13,7 @@ BEGIN
 	IF EXISTS(
 		SELECT 1 FROM INSERTED I
 		INNER JOIN Doadores D ON I.IDDoador = D.IDDoador
-		INNER JOIN PreTriagem PT ON I.IDPretriagem = PT.IDPretriagem
+		INNER JOIN PreTriagem PT ON D.IDPretriagem = PT.IDPretriagem
 		
 		WHERE I.TipoAgendamento = 'Coleta' AND PT.ResultadoPT = 'Reprovado'
 	)
@@ -29,3 +27,43 @@ BEGIN
 	SELECT IDPaciente, IDDoador, TipoAgendamento, DataHoraAgendamento, Observacoes 
 	FROM INSERTED;
 END
+
+
+--Essa trigger faz com que, se o resultado do cadastrado na pretriagem e triagem for aprovado ele ira passar para a tabela doadores automaticamente. OBS: Centralizei para a tabela cadastrados, pois ele ira interagir com doadores, triagem e pretriagem que estão relacionadas a cadastrados, então é melhor centralizar tudo logo em cadastrados 
+create trigger AprovacaoParaDoador ON Triagem
+AFTER INSERT, UPDATE
+AS
+BEGIN
+	IF EXISTS (
+		SELECT 1 FROM INSERTED I
+        
+        INNER JOIN Cadastrados C ON I.IDCadastrado = C.IDCadastrado
+        INNER JOIN PreTriagem PT ON C.IDCadastrado = PT.IDCadastrado
+        
+        WHERE C.Preferencia = 'Doador'
+			AND PT.ResultadoPT = 'Aprovado'
+			AND I.ResultadoTriagem = 'Aprovado'
+			
+			AND NOT EXISTS (
+				SELECT 1 
+				FROM Doadores D 
+				WHERE D.IDCadastrado = C.IDCadastrado
+		)
+	)
+	BEGIN
+		INSERT INTO Doadores (IDCadastrado, IDPretriagem, IDTriagem)
+		SELECT C.IDCadastrado, PT.IDPretriagem, I.IDTriagem FROM INSERTED I
+		
+		INNER JOIN Cadastrados C ON I.IDCadastrado = C.IDCadastrado
+		INNER JOIN PreTriagem PT ON C.IDCadastrado = PT.IDCadastrado
+		
+		WHERE C.Preferencia = 'Doador'
+			AND PT.ResultadoPT = 'Aprovado'
+			AND I.ResultadoTriagem = 'Aprovado'
+			AND NOT EXISTS (
+				SELECT 1 
+				FROM Doadores D 
+				WHERE D.IDCadastrado = C.IDCadastrado
+		);
+	END;
+END;
